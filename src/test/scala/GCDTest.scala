59ea6f9d47fb7fc4396ae4d22be0ff272927918e
Augie Eriksson <raeriksson@berkeley.edu>
2022-03-09 11:16:23 -0800

    added more tests

--- src/test/scala/GCDTest.scala
+++ src/test/scala/GCDTest.scala
@@ -23,5 +23,11 @@ class GCDTest extends AnyFreeSpec{
     val sim : SimulatorWrapper = new SimulatorWrapper(JavaRuntimeCompiler.compile(System.getProperty("user.dir") + "/examples/GCD.java"))
     testBehavior(sim, 9, 6, 3)
     testBehavior(sim, 4, 12, 4)
+    testBehavior(sim, 771, 880, 1)
+    testBehavior(sim, 212, 756, 4)
+    testBehavior(sim, 1, 1, 1)
+    testBehavior(sim, 1, 80, 1)
+    testBehavior(sim, 50, 1, 1)
+    testBehavior(sim, 11, 11, 11)
   }
 }
